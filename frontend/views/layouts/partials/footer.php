11e717954b41f4b8c652d4987d3f9d9efc11e8da
Hery <hery@itspku.ac.id>
2023-07-14 13:20:26 +0700

    Setup themes

--- frontend/views/layouts/partials/footer.php
+++ frontend/views/layouts/partials/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 footer-copyright text-center">
-                <p class="mb-0">Copyright 2021 © Cuba theme by pixelstrap  </p>
+                <p class="mb-0">Copyright 2021 © Epembayaran  </p>
             </div>
         </div>
     </div>
